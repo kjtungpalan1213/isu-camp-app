1b3234f66916c8c8aaf927e9b8f3c3deb50c6fa8
Billy-Jacob01 <jacobbutac01@gmail.com>
2026-09-11 15:52:37 +0800

    user session

--- lib/features/auth/services/user_session.dart
+++ lib/features/auth/services/user_session.dart
@@ -1,25 +1,22 @@
-class UserSession {
-  static String currentUsername = 'UserA1B2c3';
-  static String currentEmail = 'user@gmail.com';
-  static String studentId = '21-02384';
-  static String course = 'BS Information Technology';
-  static String college = 'CCSICT';
+  class UserSession {
+    static String currentUsername = 'UserA1B2c3';
+    static String currentEmail = 'user@gmail.com';;
 
-  static void setRegisteredUser({
-    required String username,
-    String email = '',
-  }) {
-    currentUsername = username.isNotEmpty ? username : 'UserA1B2c3';
-    if (email.isNotEmpty) currentEmail = email;
-  }
+    static void setRegisteredUser({
+      required String username,
+      String email = '',
+    }) {
+      currentUsername = username.isNotEmpty ? username : 'UserA1B2c3';
+      if (email.isNotEmpty) currentEmail = email;
+    }
 
-  static void setLoggedInUser({
-    required String username,
-  }) {
-    currentUsername = username.isNotEmpty ? username : 'UserA1B2c3';
-  }
+    static void setLoggedInUser({
+      required String username,
+    }) {
+      currentUsername = username.isNotEmpty ? username : 'UserA1B2c3';
+    }
 
-  static void logout() {
-    currentUsername = 'UserA1B2c3';
+    static void logout() {
+      currentUsername = 'UserA1B2c3';
+    }
   }
-}
